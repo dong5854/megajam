882f993f629a591a748086fbba94c7c74bcb5c21
dong young <dong5854@gmail.com>
2022-09-07 23:35:52 +0900

    first commit

---
+++ src/main/java/com/dongndong/megajam/MegaJamApplication.java
@@ -0,0 +1,13 @@
+package com.dongndong.megajam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaJamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaJamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongndong/megajam/MegajamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongndong.megajam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegajamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
