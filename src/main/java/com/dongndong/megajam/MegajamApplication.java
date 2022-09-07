aa8194247d9517f02445020c7c3f178df4b3b27b
dong young <dong5854@gmail.com>
2022-09-07 23:35:52 +0900

    first commit

---
+++ src/main/java/com/dongndong/megajam/MegajamApplication.java
@@ -0,0 +1,13 @@
+package com.dongndong.megajam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegajamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegajamApplication.class, args);
+    }
+
+}
